657c8823cf57f860cd5b71cffe65779ead9db076
Guilleplayer <guilleplayer@outlook.com>
2026-08-29 21:27:13 +0200

    1.6
    "Ajustes mejorados, ahora no es un popup lateral, es un popup a pantala completa navegable con pestañas. (Pronto se mejorará la interfaz para pantallas verticales)",
      "Nuevas opciones en Pantalla>Opciones de visualización: Tamaño de carpetas (Ajusta los accesos de la pantalla de inicio), Apps dentro de carpetas (Tamaño de texto e iconos) y Ocultar texto de carpetas (Mostrar solo iconos en la pantalla de inicio).",
      "Nueva opción en Carpetas>Preferencias: Ocultar botón cerrar de carpetas (Ocultar botón dentro de las carpetas).",
      "¡Por fin se ha arreglado el bug de la barra de búsqueda que causaba esa animación tan rara al salir! :)",
      "3 Nuevas apps (no son nuevas, pero estaban ocultas en el código): Editor de fotos, Conversor zonas horarias y Visort HTML (preview html). Además de una oculta, un editor de markdown en progreso. Apps/markdown.html.",
      "Labs ahora es visible en la carpeta sistema, pronto habrán apps disponibles dentro",
      "Ahora algo pasa si haces click varias veces en el nº de versión en ajustes.",
      "Ahora todo es un poco menos transparente que en la versión anterior",
      "Nuevo ajuste, slider de transparencia va desde 10% hasta 90%.",
      "Ahora el ícono para el PWA es el favicon normal. De momento será así, puede que luego cambie",

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que lances una nueva versión:
-const VERSION = "1.5.1.3";
+const VERSION = "1.6";
 const CACHE_NAME = `homescreen-v${VERSION}`;
 
 // Archivos esenciales para que la app cargue sin conexión
